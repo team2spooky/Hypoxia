aeeeadfbba11b20d126c8ee00a2224d0f5e7497d
Joseph Quin Rogers <jqrogers@crm.ucsc.edu>
2017-01-17 19:25:06 -0800

    Initial commit

---
+++ Source/Hypoxia/Hypoxia.h
@@ -0,0 +1,9 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __HYPOXIA_H__
+#define __HYPOXIA_H__
+
+#include "EngineMinimal.h"
+
+
+#endif
---
+++ Source/Hypoxia/HypoxiaGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameModeBase.h"
+#include "HypoxiaGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AHypoxiaGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AHypoxiaGameMode();
+};
+
+
+
