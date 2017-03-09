857b477390fa1123b41842e5108682e772d10bc8
Dylan Tran <dtra24@crm.ucsc.edu>
2017-03-08 17:45:43 -0800

    added monster idle and detect sound

--- Source/Hypoxia/HypoxiaMonster.cpp
+++ Source/Hypoxia/HypoxiaMonster.cpp
@@ -3,18 +3,22 @@
 #include "Hypoxia.h"
 #include "HypoxiaMonster.h"
 
-
 // Sets default values
 AHypoxiaMonster::AHypoxiaMonster()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+	MonsterSound = CreateDefaultSubobject<UComplexAudioComponent>(FName("Monster Sound"));
+	MonsterSound->SetupAttachment(RootComponent);
 }
 
 // Called when the game starts or when spawned
 void AHypoxiaMonster::BeginPlay()
 {
 	Super::BeginPlay();
+	MonsterSound->SetAttenuationSettings(SoundAttenuation);
+	IdleSoundTimer = IdleSoundDelay + FMath::RandRange(-0.5f, 1.0f) * IdleSoundDelayRandomness;
 }
 
 // Called every frame
@@ -22,6 +26,12 @@ void AHypoxiaMonster::Tick( float DeltaTime )
 {
 	Super::Tick( DeltaTime );
 
+	IdleSoundTimer -= DeltaTime;
+	if (IdleSoundTimer <= 0) {
+		MonsterSound->SetSound(IdleSound);
+		MonsterSound->Play();
+		IdleSoundTimer = IdleSoundDelay + FMath::RandRange(-0.5f, 1.0f) * IdleSoundDelayRandomness;
+	}
 }
 
 // Called to bind functionality to input
@@ -29,5 +39,11 @@ void AHypoxiaMonster::SetupPlayerInputComponent(UInputComponent* PlayerInputComp
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+void AHypoxiaMonster::PlayDetectSound() {
+	MonsterSound->SetSound(DetectSound);
+	MonsterSound->Play();
+	IdleSoundTimer = 10000.f;
 }
 
