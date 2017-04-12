d60e290c5f71f49d335cc743e719ab6a9aa37f1c
Quin Rogers <2quin2.rogers@gmail.com>
2017-04-12 15:13:11 -0700

    removing comments and commenting logging

--- Source/Hypoxia/HypoxiaAIController.cpp
+++ Source/Hypoxia/HypoxiaAIController.cpp
@@ -41,12 +41,12 @@ void AHypoxiaAIController::BeginPlay() {
 			//TriggerArray.Add(Trigger->GetActorLocation());
 			TArray<FVector> Data;
 			auto Comps = Trigger->GetComponents().Array();
-			UE_LOG(LogTemp, Warning, TEXT("Comps %i"), Comps.Num());
+			//UE_LOG(LogTemp, Warning, TEXT("Comps %i"), Comps.Num());
 			for (auto CompItr(Comps.CreateIterator()); CompItr; ++CompItr) {
 				auto Comp = Cast<USceneComponent>(*CompItr);
 				if (Comp->ComponentHasTag(TEXT("Base"))) {
 					TriggerArray.Add(Comp->GetComponentLocation());
-					UE_LOG(LogTemp, Warning, TEXT("CompsLocBase %f"), Comp->GetComponentLocation().X);
+					//UE_LOG(LogTemp, Warning, TEXT("CompsLocBase %f"), Comp->GetComponentLocation().X);
 					break;
 				}
 			}
@@ -54,7 +54,7 @@ void AHypoxiaAIController::BeginPlay() {
 				auto Comp = Cast<USceneComponent>(*CompItr);
 				if (Comp->ComponentHasTag(TEXT("Start"))) {
 					Data.Add(Comp->GetComponentLocation());
-					UE_LOG(LogTemp, Warning, TEXT("CompsLocStart %f"), Comp->GetComponentLocation().X);
+					//UE_LOG(LogTemp, Warning, TEXT("CompsLocStart %f"), Comp->GetComponentLocation().X);
 					break;
 				}
 			}
@@ -62,7 +62,7 @@ void AHypoxiaAIController::BeginPlay() {
 				auto Comp = Cast<USceneComponent>(*CompItr);
 				if (Comp->ComponentHasTag(TEXT("End"))) {
 					Data.Add(Comp->GetComponentLocation());
-					UE_LOG(LogTemp, Warning, TEXT("CompsLocEnd %f"), Comp->GetComponentLocation().X);
+					//UE_LOG(LogTemp, Warning, TEXT("CompsLocEnd %f"), Comp->GetComponentLocation().X);
 					break;
 				}
 			}
@@ -74,11 +74,6 @@ void AHypoxiaAIController::BeginPlay() {
 			//UE_LOG(LogTemp, Warning, TEXT("CompsLoc %f"), TriggerData[0][1].X);
 		}
 		
-		/*ATriggerBox *Trigger = *ActorItr;
-		TriggerArray.Add(Trigger->GetActorLocation());
-		TArray<float> Data;
-		Data.Init(Trigger->Tags);
-		TriggerData.Add(Data);*/
 	}
 
 	Goal = EGoal::Idle;
@@ -90,13 +85,6 @@ void AHypoxiaAIController::BeginPlay() {
 	InvestigateTime = 0.0f;
 	WanderTime = 0.0f;
 	WanderDistance = 1000.0f;
-
-	//UE_LOG(LogTemp, Warning, TEXT("LoS %d"), LineOfSightTo(HypoxiaCharacter));
-
-	//UBlackboardComponent* AIBlackboard = UAIBlueprintHelperLibrary::GetBlackboard(this);
-
-	//AIBlackboard->SetValueAsVector(FName("WanderLocation"), FVector(1050.0f, 500.0f, 90.0f));
-	//AIBlackboard->SetValueAsVector(FName("WanderLocation"), FVector(-700.0f, -1040.0f, 118.0f));
 
 }
 
@@ -145,88 +133,6 @@ void AHypoxiaAIController::Tick(float DeltaTime) {
 
 	UBlackboardComponent* AIBlackboard = UAIBlueprintHelperLibrary::GetBlackboard(this);
 	AIBlackboard->SetValueAsVector(FName("WanderLocation"), GoalPoint);
-
-	////UE_LOG(LogTemp, Warning, TEXT("Dist %F"), FVector::Dist(HypoxiaCharacter->GetActorLocation(), GetPawn()->GetActorLocation()));
-	////UE_LOG(LogTemp, Warning, TEXT("LoS %d"), LineOfSightTo(HypoxiaCharacter) && FVector::Dist(HypoxiaCharacter->GetActorLocation(), GetPawn()->GetActorLocation()) < 200.0f);
-	//UBlackboardComponent* AIBlackboard = UAIBlueprintHelperLibrary::GetBlackboard(this);
-	//APawn* AIPawn = GetPawn();
-	////UE_LOG(LogTemp, Warning, TEXT("Player X %f"), HypoxiaCharacter->GetActorLocation().X);
-	////UE_LOG(LogTemp, Warning, TEXT("Player Y %f"), HypoxiaCharacter->GetActorLocation().Y);
-	////UE_LOG(LogTemp, Warning, TEXT("Monster Z %f"), pawn->GetActorLocation().Z);
-	//if (LineOfSightTo(HypoxiaCharacter) && FVector::Dist(HypoxiaCharacter->GetActorLocation(), GetPawn()->GetActorLocation()) < 200.0f) {
-	//	AIBlackboard->SetValueAsBool(FName("SeenPlayer"), true);
-	//}
-	//if (AIBlackboard->GetValueAsBool(FName("SeenPlayer"))) {
-	//	AIBlackboard->SetValueAsVector(FName("PlayerLocation"), HypoxiaCharacter->GetActorLocation());
-	//}
-	//AIBlackboard->SetValueAsBool(FName("StartMonster"), true);
-	//if (AIBlackboard->GetValueAsBool(FName("StartMonster"))) {
-	//	MoveTimer -= DeltaTime;
-	//	Looktime -= DeltaTime;
-	//	WanderTimer -= DeltaTime;
-	//	if (Looktime <= 0.0f) {
-	//		AIBlackboard->SetValueAsBool(FName("HeardSound"), false);
-	//	}
-	//	//RandNum = FMath::RandRange(1, 5);
-	//	//RandNum = 6;
-	//	/*if (MoveTimer <= 0.0f) {
-	//		MoveTimer = 20.0f;
-	//		UE_LOG(LogTemp, Warning, TEXT("Rand %d"), RandNum);
-	//		switch (RandNum) {
-	//		case 1:
-	//			AIBlackboard->SetValueAsVector(FName("WanderLocation"), FVector(MPoint1X, MPoint1Y, MPoint1Z));
-	//			break;
-	//		case 2:
-	//			AIBlackboard->SetValueAsVector(FName("WanderLocation"), FVector(MPoint2X, MPoint2Y, MPoint2Z));
-	//			break;
-	//		case 3:
-	//			AIBlackboard->SetValueAsVector(FName("WanderLocation"), FVector(MPoint3X, MPoint3Y, MPoint3Z));
-	//			break;
-	//		case 4:
-	//			AIBlackboard->SetValueAsVector(FName("WanderLocation"), FVector(MPoint4X, MPoint4Y, MPoint4Z));
-	//			break;
-	//		case 5:
-	//			AIBlackboard->SetValueAsVector(FName("WanderLocation"), FVector(MPoint5X, MPoint5Y, MPoint5Z));
-	//			break;
-	//		default:
-	//			AIBlackboard->SetValueAsVector(FName("WanderLocation"), FVector(FMath::RandRange(1810.f, 4880.0f), FMath::RandRange(-1770.f, 430.0f), 20.0f));
-	//			UE_LOG(LogTemp, Warning, TEXT("Path"));
-	//			break;
-	//		}
-	//	}*/
-	//	UE_LOG(LogTemp, Warning, TEXT("Dist %f"), FVector::Dist(AIBlackboard->GetValueAsVector(FName("WanderLocation")), GetPawn()->GetActorLocation()));
-	//	if (MoveTimer <= 0.0f) {
-	//		AIBlackboard->SetValueAsVector(FName("WanderLocation"), FVector(FMath::RandRange(1810.f, 4880.0f), FMath::RandRange(-1770.f, 430.0f), 70.0f));
-	//		MoveTimer = 20.0f;
-	//	} else if (FVector::Dist(AIBlackboard->GetValueAsVector(FName("WanderLocation")), GetPawn()->GetActorLocation()) < 200.0f){
-	//		FVector NewWander = FVector(AIBlackboard->GetValueAsVector(FName("WanderLocation")).X + FMath::RandRange(-10.0f, 10.0f), AIBlackboard->GetValueAsVector(FName("WanderLocation")).Y + FMath::RandRange(-10.0f, 10.0f), AIBlackboard->GetValueAsVector(FName("WanderLocation")).Z + FMath::RandRange(-10.0f, 10.0f));
-	//		AIBlackboard->SetValueAsVector(FName("WanderLocation"), FVector());
-	//	}
-	//}
-	////	if (AIBlackboard->GetValueAsBool(FName("AtWanderLocation"))) {
-	////		UE_LOG(LogTemp, Warning, TEXT("Monster's moving around the Location"));
-	////		TempX = AIBlackboard->GetValueAsVector(FName("WanderLocation")).X;
-	////		TempY = AIBlackboard->GetValueAsVector(FName("WanderLocation")).Y;
-	////		TempX += FMath::RandRange(-75.0f, 75.0f);
-	////		TempY += FMath::RandRange(-75.0f, 75.0f);
-	////		AIBlackboard->SetValueAsVector(FName("WanderLocation"), FVector(TempX, TempY, 90));
-	////		AIBlackboard->SetValueAsBool(FName("AtWanderLocation"), false);
-	////		WanderTimer = 3.0f;
-	////	} else if ((Pawn->GetActorLocation().X >= AIBlackboard->GetValueAsVector(FName("WanderLocation")).X - 50.0f)
-	////		&& (Pawn->GetActorLocation().X <= AIBlackboard->GetValueAsVector(FName("WanderLocation")).X + 50.0f)
-	////		&& (WanderTimer <= 0.0f)
-	////		//&& (pawn->GetActorLocation().Z <= AIBlackboard->GetValueAsVector(FName("WanderLocation")).Z + 1.0f)
-	////		//&& (pawn->GetActorLocation().Z >= AIBlackboard->GetValueAsVector(FName("WanderLocation")).Z - 71.0f)) {
-	////		) {
-	////		AIBlackboard->SetValueAsBool(FName("AtWanderLocation"), true);
-	////		UE_LOG(LogTemp, Warning, TEXT("Monster's at the Location"));
-	////	}
-	////} else if ((HypoxiaCharacter->GetActorLocation().X >= TriggerPtX1)
-	////	&& (HypoxiaCharacter->GetActorLocation().X <= TriggerPtX2)
-	////	&& (HypoxiaCharacter->GetActorLocation().Y <= TriggerPtY1)
-	////	&& (HypoxiaCharacter->GetActorLocation().Y >= TriggerPtY2)) {
-	////	UE_LOG(LogTemp, Warning, TEXT("Monster's Moving Around"));
-	////	AIBlackboard->SetValueAsBool(FName("StartMonster"), true);
 
 }
 
@@ -280,8 +186,8 @@ FVector AHypoxiaAIController::NextWanderPoint() {
 
 	FVector NewWanderPoint = FVector(PlayerLocation.X + XDist, PlayerLocation.Y + YDist, 450.0f);
 
-	UE_LOG(LogTemp, Warning, TEXT("XPoint %f"), PlayerLocation.X + XDist);
-	UE_LOG(LogTemp, Warning, TEXT("YPoint %f"), PlayerLocation.Y + YDist);
+	//UE_LOG(LogTemp, Warning, TEXT("XPoint %f"), PlayerLocation.X + XDist);
+	//UE_LOG(LogTemp, Warning, TEXT("YPoint %f"), PlayerLocation.Y + YDist);
 
 	WanderTime = 10.0f;
 
