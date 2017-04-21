7b5eaf9a8a848ede7043655862d0ba5f7169cac6
Quin Rogers <2quin2.rogers@gmail.com>
2017-04-20 17:40:45 -0700

    minor fixes

--- Source/Hypoxia/HypoxiaAIController.cpp
+++ Source/Hypoxia/HypoxiaAIController.cpp
@@ -206,6 +206,6 @@ void AHypoxiaAIController::HearSound(FVector Location, float Amplitude) {
 
 	FVector MonsterLocation = GetPawn()->GetActorLocation();
 
-	GetPawn()->SetActorLocation(FVector(MonsterLocation.X, MonsterLocation.Y, 75.0f));
+	//GetPawn()->SetActorLocation(FVector(MonsterLocation.X, MonsterLocation.Y, 75.0f));
 
 }
