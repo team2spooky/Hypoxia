e9202ae8f99317d6d055bb723921c8ca07620c32
kytmatsu <kyletmatsumoto@gmail.com>
2017-05-26 17:40:52 -0700

    Portal
    Teleport works and monster spawns at Corridor

--- Source/Hypoxia/HypoxiaAIController.cpp
+++ Source/Hypoxia/HypoxiaAIController.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+w// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Hypoxia.h"
 #include "HypoxiaAIController.h"
